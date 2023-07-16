8b647c786d9a638dfba5bfd6e513b0d68d748df6
AhmetZinavur <ahmetzinavur@gmail.com>
2023-07-16 14:46:11 +0300

    updated

--- src/age_calculation/App.java
+++ src/age_calculation/App.java
@@ -3,27 +3,29 @@ package age_calculation;
 import java.util.Scanner;
 
 public class App {
-    
+
     public static void main(String[] args) {
-        
+
         int day;
         int month;
         int year;
-        
+
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Hangi Yılda Doğdunuz : ");
-        year = input.nextInt();
-        
-        System.out.print("Kaçıncı Ayda Doğdunuz : ");
-        month = input.nextInt();
-        
-        System.out.print("Ayın Kaçında Doğdunuz : ");
+        System.out.println("Bu uygulama yaşınızı hesaplamaktadır.");
+
+        System.out.print("Gün : ");
         day = input.nextInt();
-        
+
+        System.out.print("Ay : ");
+        month = input.nextInt();
+
+        System.out.print("Yıl: ");
+        year = input.nextInt();
+
         Age myAge = new Age();
-        
+
         System.out.println(myAge.calculateAge(year, month, day) + " Yaşındasınız");
-        
+
     }
 }
